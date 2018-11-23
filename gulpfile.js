439e9aad92ce70d3a8d4ceac44fb9fdf8dd76b9e
Alfonzo Angulo de la Cruz <aangulodelacruz@calgarylibrary.ca>
2018-11-23 09:16:43 -0700

    Added CoffeScript files

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,11 @@
 var gulp = require('gulp'),
-    gutil = require('gulp-util');
+    gutil = require('gulp-util'),
+    coffee = require('gulp-coffee');
+var coffeeSourcess = ['components/coffee/tagline.coffe'];
 
-gulp.task('log', function () {
-    gutil.log('Hello');
-})
+gulp.task('coffee', function () {
+    gulp.src(coffeeSourcess)
+        .pipe(coffee({ bare: true })
+            .on('error', gutil.log))
+        .pipe(gulp.dest('components/scripts'))
+});
